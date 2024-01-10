1cc9c40f4e349c782c7fa224fae7acea6c4981fe
xoventechdev <alkarim.doha@gmail.com>
2024-01-10 22:20:08 +0300

    Translate spelling

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ async function fetchData() {
     Revelation Type : ${quranData.data[0].surah.revelationType}</br>`;
 
     qurandetails.innerHTML = `<h5>"${quranData.data[0].text}"</h5>${quranData.data[0].surah.englishName} - ${quranData.data[0].numberInSurah}</br>
-    Translatte by ${quranData.data[0].edition.englishName}</br><br>`;
+    Translate by ${quranData.data[0].edition.englishName}</br><br>`;
     
 }
 
