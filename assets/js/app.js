4ef9f7dbe6d4dd3ffef50bf9c018684b95d89573
xoventechdev <alkarim.doha@gmail.com>
2024-01-10 18:13:47 +0300

    basic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,19 @@
-let bookname  = document.querySelector('#bookname');
-let bookauthor = document.querySelector('#author');
-let publishdate = document.querySelector('#publishdate');
-let booktype = document.querySelector('#type');
-let addbook = document.querySelector('#add-book');
-let booklist = document.querySelector('#book-list');
+let quraninfo = document.querySelector(".quran-info");
+let qurandetails = document.querySelector(".quran-details");
 
-addbook.addEventListener('click', function(){
-    if (bookname.value == '' || bookauthor.value == '' || publishdate.value == '' || booktype.value == '') {
-        alert('Please fill all the fields');
-    }else{
-        const bookrow = document.createElement('tr');
-        const booknamecell = document.createElement('td');
-        booknamecell.textContent = bookname.value;
-        const bookauthorcell = document.createElement('td');
-        bookauthorcell.textContent = bookauthor.value;
-        const publishdatecell = document.createElement('td');
-        publishdatecell.textContent = publishdate.value;
-        const booktypecell = document.createElement('td');
-        booktypecell.textContent = booktype.value;
-        const deletebtn = document.createElement('button');
-        deletebtn.textContent = 'Delete';
-        deletebtn.classList.add('btn', 'btn-warning');
-        deletebtn.addEventListener('click', function(){
-            this.parentElement.parentElement.remove();
-        })
-        bookrow.appendChild(booknamecell);
-        bookrow.appendChild(bookauthorcell);
-        bookrow.appendChild(publishdatecell);
-        bookrow.appendChild(booktypecell);
-        booklist.appendChild(bookrow);
-        bookrow.appendChild(deletebtn);
-        bookname.value = '';
-        bookauthor.value = '';
-        publishdate.value = '';
-        booktype.value = '';
+// let getQuran = fetch("https://api.alquran.cloud/v1/ayah/262/editions/en.pickthall");
+// let quranData = await getQuran.then((res) => res.json());
+// console.log(quranData);
 
+
+async function fetchData() {
+    try {
+        let getQuran = await fetch("https://api.alquran.cloud/v1/ayah/262/editions/en.pickthall");
+        let quranData = await getQuran.json();
+        console.log(quranData);
+    } catch (error) {
+        console.error('Error fetching Quran data:', error);
     }
-})
+}
+
+fetchData();
