acb8749a6c0bebf735c61afe7b9a9d7ded5a30ca
xoventechdev <alkarim.doha@gmail.com>
2024-01-11 09:05:02 +0300

    refresh button added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,15 @@
 let quraninfo = document.querySelector(".quran-info");
 let qurandetails = document.querySelector(".quran-details");
-let quranMp3 = document.querySelector("#quran-mp3");
+let audioDiv = document.querySelector(".mp3");
+let shareDiv = document.querySelector(".card-body");
 
-
-let randomNumber = Math.floor(Math.random() * 6236) + 1;
 
 async function fetchData() {
+    
+    let randomNumber = Math.floor(Math.random() * 6236) + 1;
+
     let getQuran = await fetch("https://api.alquran.cloud/v1/ayah/"+randomNumber+"/editions/en.pickthall");
     let quranData = await getQuran.json();
-
 
     quraninfo.innerHTML = `<h4>${quranData.data[0].surah.englishName}</h4>
     English Mean : ${quranData.data[0].surah.englishNameTranslation} | 
@@ -17,20 +18,23 @@ async function fetchData() {
 
     qurandetails.innerHTML = `<h5>"${quranData.data[0].text}"</h5>${quranData.data[0].surah.englishName} - ${quranData.data[0].numberInSurah}</br>
     Translate by ${quranData.data[0].edition.englishName}</br><br>`;
+
+
+    let getMp3 = await fetch("https://api.alquran.cloud/v1/ayah/"+randomNumber+"/ar.alafasy");
+    let quranaudio = await getMp3.json();
+
+    audioDiv.innerHTML = `<audio controls>
+    <source src="${quranaudio.data.audio}" type="audio/mpeg">
+    Your browser does not support the audio element.
+    </audio>`;
     
 }
 
-async function fetchAudio() {
+function shareImage(){
     
-    let getMp3 = await fetch("https://api.alquran.cloud/v1/ayah/"+randomNumber+"/ar.alafasy");
-    let quranaudio = await getMp3.json();
-    console.log(quranaudio)
-
-    quranMp3.src = quranaudio.data.audio;
 
 }
 
 
 
 fetchData();
-fetchAudio();
